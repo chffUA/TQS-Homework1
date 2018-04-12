e1cc20abd82f1917e9214394ff25895d0ddebf2c
chffUA <32465991+chffUA@users.noreply.github.com>
2018-04-12 15:07:47 +0100

    Update ConvBeanTest.java

--- src/test/java/com/mycompany/tqs_hw1/ConvBeanTest.java
+++ src/test/java/com/mycompany/tqs_hw1/ConvBeanTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.mycompany.tqs_hw1;
 
 import java.util.List;
@@ -16,7 +11,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author Asus
+ * @author Carlos
  */
 public class ConvBeanTest {
     
